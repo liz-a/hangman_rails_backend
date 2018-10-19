f05da5adfbd38b59793aedb9c7d1f3573c81ecc2
Liz Ardolino <lizardolino@Lizs-MacBook-Pro.local>
2018-10-19 15:02:48 +0100

    setup slack endpoint

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -1,9 +1,10 @@
 class SlackController < ApplicationController
   skip_before_action :verify_authenticity_token
-  def process
+  def get_input
     Rails.logger.info '*' * 100
     Rails.logger.info params.inspect
     Rails.logger.info '*' * 100
 
+    head :ok
   end
 end
