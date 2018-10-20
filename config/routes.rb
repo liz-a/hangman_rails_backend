96df7e7b85079e79f602f2975e050340722b53e8
Liz Ardolino <lizardolino@Lizs-MacBook-Pro.local>
2018-10-20 14:52:47 +0100

    end of friday

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   resources :players
   resources :games
   post "/slack", to: "slack#get_input"
+
+  # post "/slack", to: "games#create", constraint: SlackCreateGameConstraint.new
+  # post "/slack", to: "guesses#update", constraint: SlackGuessConstraint.new
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "homepage#show"
 
