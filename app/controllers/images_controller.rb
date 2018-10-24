42f17774da3b8876cf82d24577c48929b6befa4c
Liz Ardolino <lizardolino@Lizs-MacBook-Pro.local>
2018-10-24 16:16:59 +0100

    images added

---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,34 @@
+class ImagesController < ApplicationController
+  before_action :set_player, only: [:show, :edit, :update, :destroy]
+  skip_before_action :verify_authenticity_token
+
+  def i13
+  end 
+  def i12
+  end 
+  def i11
+  end 
+  def i10
+  end 
+  def i9
+  end 
+  def i8
+  end 
+  def i7
+  end 
+  def i6
+  end 
+  def i5
+  end 
+  def i4
+  end 
+  def i3
+  end 
+  def i2
+  end 
+  def i1
+  end 
+  def i0
+  end 
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,21 @@ Rails.application.routes.draw do
 
   get "/players/exists/:slack_id", to: "players#exists"
 
+  get "images/13", to: "images#i13"
+  get "images/12", to: "images#i12"
+  get "images/11", to: "images#i11"
+  get "images/10", to: "images#i10"
+  get "images/9", to: "images#i9"
+  get "images/8", to: "images#i8"
+  get "images/7", to: "images#i7"
+  get "images/6", to: "images#i6"
+  get "images/5", to: "images#i5"
+  get "images/4", to: "images#i4"
+  get "images/3", to: "images#i3"
+  get "images/2", to: "images#i2"
+  get "images/1", to: "images#i1"
+  get "images/0", to: "images#i0"
+
   # post "/slack", to: "slack#get_input"
 
   # post "/slack", to: "games#create", constraint: SlackCreateGameConstraint.new
